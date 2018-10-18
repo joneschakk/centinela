d8ae06a9b42dd2e7173b6b60c1dcf59c6e46a987
arun.muraleedharan <arun.muraleedharan@flytxt.com>
2018-10-18 10:25:37 +0530

    wip

--- main.go
+++ main.go
@@ -10,16 +10,60 @@ import (
 )
 
 func authenticateHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	r.ParseForm()
+
+	if r.Method == "OPTIONS" { // CORS preflight
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		w.WriteHeader(200)
+		return
+	}
+
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed) // 405
+		return
+	}
+
+	authFor := r.Header.Get("X-Auth-For")
+	if authFor == "" {
+		w.WriteHeader(406) // TODO change
+		fmt.Fprintf(w, "Authentication target is empty")
+		return
+	}
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	fmt.Println("authenticating ..... | " + username + ":" + password)
+
+	if username == "u" && password == "p" {
+		http.Redirect(w, r, authFor, http.StatusTemporaryRedirect)
+		return
+	}
+	w.WriteHeader(http.StatusBadRequest)
+	fmt.Fprintf(w, "Authentication failed")
 }
 
 func loginPageHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
+
 func authenticatedCheckHandler(w http.ResponseWriter, r *http.Request) {
-	// r.Response.StatusCode = 500
-	w.WriteHeader(200)
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	for k, v := range r.Header {
+		fmt.Println("k " + k + " v: " + v[0])
+	}
+	authFor := r.Header.Get("X-Auth-For")
+	authToken := r.Header.Get("X-Auth-Token")
+	if authFor == "" {
+		w.WriteHeader(400)
+		fmt.Fprintf(w, "Authentication target is empty")
+		return
+	}
+	if authToken == "foo" {
+		w.WriteHeader(200)
+		fmt.Fprintf(w, "Authorized")
+		return
+	}
+	w.WriteHeader(401)
+	fmt.Fprintf(w, "Not authorized")
 }
 
 func main() {
@@ -36,5 +80,5 @@ func main() {
 	http.Handle("/login/", http.StripPrefix("/login/", http.FileServer(statikFS)))
 
 	fmt.Println("starting centinela server...")
-	log.Fatal(http.ListenAndServe(":9999", nil))
+	log.Fatal(http.ListenAndServe(":6969", nil))
 }
